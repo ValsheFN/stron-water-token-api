4365e47ad07143b2a31c7eba15164ecf02fccc7e
Filbert Nicholas <filbert@scroll.io>
2025-06-08 19:35:21 +0700

    Add cron job

--- src/server.js
+++ src/server.js
@@ -5,10 +5,13 @@ import rateLimiter from "./middleware/rate_limiter.js";
 
 import transactions from "./routes/transactions.js"
 import meter from "./routes/meter.js"
+import job from "./config/cron.js"
 
 dotenv.config();
 
 const app = express();
+
+if(process.env.NODE_ENV === "production") job.start();
 
 //middleware
 app.use(rateLimiter);
@@ -16,10 +19,11 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
+
 // Basic health check route
-app.get('/', (req, res) => {
-    res.json({ message: 'Server is running!', timestamp: new Date().toISOString() });
-});
+app.get('/api/health', (req, res) => {
+    res.status(200).json({status: "ok"})
+})
 
 // API routes
 app.use("/api/transactions", transactions);
